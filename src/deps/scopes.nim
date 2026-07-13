0711029c0677d0d851d1ad2ea43ad135a3b01b05
Talom Lael <gesee37@gmail.com>
2026-07-13 16:32:15 +0100

    Finalized scope implementation

--- src/deps/scopes.nim
+++ src/deps/scopes.nim
@@ -1,4 +1,5 @@
-
+import strutils
+import trie
 
 type
   Liveness = object
@@ -10,9 +11,11 @@ type
     name: string
     type: TypeInst
     liveness: Liveness
+    state: BorrowState
 
   ScopeNode = ref object
-    variables: Table[string, Variable]
+    id: int
+    variables: Trie[Variable]
     children: seq[ScopeNode]
     moveState: Table[SymPath, BorrowState]
     inlineVars: Table[SymId, NifCursor]
@@ -22,6 +25,17 @@ type
 # ############################################################################################################ #
 # ############################################### Functions ################################################## #
 # ############################################################################################################ #
+
+proc getOwnerScope(c: ScopeNode, path: seq[string]): ScopeNode =
+  result = scope
+  while scope.variables.fetchNode(path) == -1 and result.parent != nil:
+    result = result.parent
+
+proc getOwnerScope(c: ScopeNode, path: string): ScopeNode =
+  return c.getOwnerScope(path.split("."))
+
+proc getOwnerScope(c: ScopeNode, path: SymPath): ScopeNode =
+  return c.getOwnerScope(renderPath(path))
 
 proc extractPath(c: var ScopeNode; n: NifCursor; res: var SymPath; followInlineVars = true) =
   if not res.valid:
@@ -82,24 +96,26 @@ proc collectVarData(scope: var ScopeNode, root: NifCursor, cache: TypeCache) =
     if path.valid and path.path.len > 0:
       let strPath = renderPath(path)
 
-      var ownerScope = scope
-      while root notin scope.moveState and ownerScope.parent != nil:
-        ownerScope = ownerScope.parent
-
-      ownerScope.moveState[path] = BorrowState(kind: Alive)
-      let rv = ownerScope.variables.getOrDefault(root)
+      var ownerScope = scope.getOwnerScope(root)
+      let rv = ownerScope.variables.fetchNode(root)
+      var id = -1
 
       if strPath in ownerScope.variables:
         try:
-          ownerScope.variables[strPath].liveness.uses.add(n)
+          id = ownerScope.variables.fetchNode(strPath.split("."))
+          ownerScope.variables.nodes[id].data.liveness.uses.add(n)
         else:
           discard
       else:
-        var v = Variable(name: strPath, kind: rv.kind, ty: cache.getType(rv.ty, strPath.split(".")[1..^1]),
+        var v = Variable(name: strPath, kind: ownerScope.variables.node[rv].data.kind,
+          ty: cache.getType(ownerScope.variables.node[rv].data.ty, strPath.split(".")[1..^1]),
           liveness: Liveness(birth: n))
         v.liveness.uses.add(n)
-        ownerScope.variables[strPath] = v
 
+        id = ownerScope.variables.addNode(strPath.split("."))
+        ownerScope.variables.node[id].data = v
+
+      ownerScope.variables.nodes[id].data.state = BorrowState(kind: Alive)
 
   n.loopInto:
     case n.stmtKind
@@ -113,9 +129,10 @@ proc collectVarData(scope: var ScopeNode, root: NifCursor, cache: TypeCache) =
         skip typ
         let inst = cache.getType(typ.symText)
         let v = Variable(kind: kind, ty: inst, name: symNode.name, liveness: Liveness(birth: n))
-        scope.declTypes[symNode.symId] = typ
+        let id = current.addNode(name)
+        current.variables.nodes[id].data = v
     of StmtsS, BlockS:
-      var newNode = ScopeNode()
+      var newNode = ScopeNode(id: current.children.len)
       newNode.parent = current
       current.children.add(newNode)
       current = newNode
@@ -127,3 +144,126 @@ proc collectVarData(scope: var ScopeNode, root: NifCursor, cache: TypeCache) =
       if current.parent != nil:
         current = current.parent
     n.skip()
+
+# ######################################################################################################### #
+# ############################################# Move Checks ############################################### #
+# ######################################################################################################### #
+
+proc movePathsWithPrefix(ctx: var ScopeNode; prefix: SymPath, n: NifCursor) =
+  ## When a path (or its root) is mutated, every fact about a longer path that
+  ## shares this prefix (e.g. `a.next` when `a` is reassigned) is stale.
+  let data = renderPath(prefix).split(".")
+  for id in ctx.variables.iterateSuffix(data):
+    ctx.variables.nodes[id].data.state.kind = Moved
+    ctx.variables.nodes[id].data.state.pos = n
+
+proc relivePathsWithPrefix(ctx: var ScopeNode; prefix: SymPath, n: NifCursor) =
+  ## When a path (or its root) is mutated, every fact about a longer path that
+  ## shares this prefix (e.g. `a.next` when `a` is reassigned) is stale.
+  let data = renderPath(prefix).split(".")
+  for id in ctx.variables.iterateSuffix(data):
+    ctx.variables.nodes[id].data.state.kind = Alive
+
+proc isAnyDescendantMoved(ctx: ScopeNode, path: SymPath): bool =
+  ## Check if any var with prefix `path` has been moved.
+  ## This is to check if an object can be moved even if it's currently alive
+  ## Because it's a partial object
+  let data = renderPath(prefix).split(".")
+  for id in ctx.variables.iterateSuffix(data):
+    if ctx.variables.nodes[id].data.state.kind == Moved:
+      return true
+
+  return false
+
+proc checkPath(ctx: var ScopeNode, r: var Replacer, path: SymPath, isAssign=NoAsgn, isLet=false) =
+  var n = r.getCursor()
+  let info = n.info
+  if path.valid and path.path.len > 0:
+    let sym = path.path[0]
+    let strPath = renderPath(path)
+    var ownerScope = ctx.getOwnerScope(strPath)
+    var strSPath = strPath.split(".")
+    let id = ownerScope.fetchNode(strSPath)
+    let vk = ownerScope.variables.nodes[id].data.kind
+    let l = ownerScope.variables.nodes[id].data.liveness
+    let varType = ownerScope.variables.nodes[id].data.ty
+
+    var l = ctx.lifetimes.getOrDefault(sym)
+    let vk = ctx.varKinds.getOrDefault(sym)
+
+    if isAssign == LHSAsgn and l.creation != info:
+      if vk == LetK:
+        ctx.errorStack.add errorInstance("Can't modifiy an immutable let variable", n, n)
+      else:
+        # Fix TRevive
+        # If it's an assignment and the LHS receive a new value but is not LetK
+        # Then it revive it
+        ownerScope.variables.nodes[id].data.state = BorrowState(kind: Alive)
+        relivePathsWithPrefix ownerScope, path, n
+
+    # If we are assigning, we are on the RHS which has been declared as let
+    # And try to assign it to a var variable while the data we are trying to assign is not a ref
+    elif isAssign == RHSAsgn and vk == LetK and not isLet and varType.kind notin {ObjectType, PrimitiveType}:
+      ctx.errorStack.add errorInstance("Can't assign an immutable let variable to a var.", n, n)
+
+    # Here we check is anything on the path to the variable is already moved
+    # And prevent use after move
+    let moved = ownerScope.variables.nodes[id].data.state.kind == Moved
+    if isAnyMovedInPath(ctx, path) or moved:
+      ctx.errorStack.add errorInstance("Used after move here", r.getCursor, moved.pos)
+      return
+
+    if isAssign == RHSAsgn:
+      ownerScope.variables.nodes[id].data.state = BorrowState(kind: Moved, pos: n)
+      movePathsWithPrefix ownerScope, path, n
+
+proc checkMoves(r: var Replacer; ctx: var ScopeNode, isAssign = NoAsgn, isLet=false) =
+  var current = 0
+  var newScope = false
+  if r.isAtom:
+    let n = r.getCursor
+    if n.kind == Symbol:
+      let path = extractPath(ctx, n)
+      checkPath(ctx, r, path, isAssign=isAssign, isLet=isLet)
+    keep r, Any
+  else:
+    if r.kind == TagLit:
+      case r.exprKind
+      of HderefX, DotX, DdotX, HaddrX, AddrX, DerefX, TupatX, ArratX, AtX, PatX:
+        var n = r.getCursor()
+        let info = n.info
+        let path = extractPath(ctx, r.getCursor)
+        checkPath(ctx, r, path, isAssign=isAssign, isLet=isLet)
+      else:
+        discard
+    case r.stmtKind
+    of StmtsS, BlockS:
+      newScope = true
+      var parent = ctx.parent
+      if parent != nil:
+        loopKeepTag r:
+          checkMoves(r, parent.children[ctx.id+1])
+
+      current += 1
+    of AsgnS:
+      loopKeepTag r:
+        checkMoves(r, ctx, isAssign = LHSAsgn)
+        checkMoves(r, ctx, isAssign = RHSAsgn)
+    of CommentS:
+      var stmt = createTree()
+      for err in ctx.errorStack:
+        stmt.addTree errorTree(err.msg, err.at, err.orig)
+      replace r, Any, stmt
+    of VarS, LetS:
+      var cnt = 0
+      let isLet = r.stmtKind == LetS
+      loopKeepTag r:
+        checkMoves(r, ctx, isAssign = if cnt > 0: RHSAsgn else: NoAsgn, isLet=isLet)
+        cnt += 1
+    else:
+      loopKeepTag r:
+        checkMoves(r, ctx)
+
+    if newScope:
+      if current.parent != nil:
+        current = current.parent
--- src/deps/trie.nim
+++ src/deps/trie.nim
@@ -5,6 +5,7 @@
 type
   TNode[T] = object
     data: T
+    parent: int
     children: Table[string, int]
 
   Trie[T] = object
@@ -12,10 +13,22 @@ type
     roots: TNode[T]
 
 proc newTrie[T](): Trie[T] =
-  result = Trie[T]()
+  result = Trie[T](parent: -1)
   result.nodes.add(default(TNode[T]))
 
-proc addNode[T](t: Trie[T], data: seq[string]) =
+proc addNode[T](t: Trie[T], data: string): int =
+  var root = 0
+  if data in t.nodes[root].children:
+    root = t.nodes[root].children.getOrDefault(data)
+  else:
+    let n = t.nodes.len
+    t.nodes.add(TNode[T](name: data, parent: root))
+    t.nodes[root].children[data] = n
+    root = n
+
+  return root
+
+proc addNode[T](t: Trie[T], data: seq[string]): int =
   if data.len <= 0: return
 
   var root = 0
@@ -24,9 +37,11 @@ proc addNode[T](t: Trie[T], data: seq[string]) =
       root = t.nodes[root].children.getOrDefault(d)
     else:
       let n = t.nodes.len
-      t.nodes.add(TNode[T](name: d))
+      t.nodes.add(TNode[T](name: d, parent: root))
       t.nodes[root].children[d] = n
       root = n
+
+  return root
 
 proc fetchNode[T](t: Trie[T], data: seq[string]): int =
   if data.len <= 0: return -1
@@ -34,6 +49,9 @@ proc fetchNode[T](t: Trie[T], data: seq[string]): int =
   result = 0
   for d in data:
     result = t.nodes[result].children.getOrQuit(d)
+
+proc fetchNode[T](t: Trie[T], data: string): int =
+  result = t.nodes[0].children.getOrDefault(data, -1)
 
 proc getAllSuffix[T](t: Trie[T], node: TNode[T], stack: var seq[int]): int =
   for v in node.children.values:
