71b2fbb7f9974238ee059c179b54010cc3410e8c
Talom Lael <gesee37@gmail.com>
2026-07-20 15:47:32 +0100

    Added test

---
+++ tests/tclosure.nim
@@ -0,0 +1,16 @@
+import ../src/borrowing
+import std/syncio
+
+enableBorrowChecker()
+
+type
+  RefO = ref object
+    x: int
+
+proc main() =
+  var p = RefO()
+  var n = proc () =
+    var x = p
+  echo p.x
+
+main()
